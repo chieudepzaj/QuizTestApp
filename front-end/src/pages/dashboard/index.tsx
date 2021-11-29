b50d71a3173388bd5ecdb0d1f2adae099da7d931
Hoang993007 <nguyenminhhoang1999.tg@gmail.com>
2021-11-29 20:45:08 +0700

    fix take test page

--- front-end/src/pages/dashboard/index.tsx
+++ front-end/src/pages/dashboard/index.tsx
@@ -14,7 +14,7 @@ const Dashboard: React.FC = () => {
 
   return (
     <>
-      {!user.fullname && <Navigate to={routePath.PROFILE} />}
+      {user.accessToken && !user.fullname && <Navigate to={routePath.PROFILE} />}
 
       <Header />
       <div className="dashboard">
