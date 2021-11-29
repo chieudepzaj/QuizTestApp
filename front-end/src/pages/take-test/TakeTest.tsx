9e98aabedbdd4489a6077cd5921f1700cfa4698d
unknown <artogirus@gmail.com>
2021-11-29 17:23:58 +0700

    Update

--- front-end/src/pages/take-test/TakeTest.tsx
+++ front-end/src/pages/take-test/TakeTest.tsx
@@ -1,15 +1,17 @@
 import { Button } from 'antd';
 import React from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { UserRole } from 'src/constants/constants';
 import routePath from 'src/constants/routePath';
 import Header from 'src/layouts/header';
 import './styles.scss';
 import quizImg from 'src/assets/images/quiz.png';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { UserRole } from 'src/constants/constants';
 import { useAppSelector } from 'src/store/hooks';
 
 
 const TakeTest: React.FC = () => {
+  const user = useAppSelector((user) => user.account.user);
+  const navigate = useNavigate();
   return <>
   <Header />
   <div className='container'>
@@ -18,9 +20,11 @@ const TakeTest: React.FC = () => {
     <div className='box'>
       <img className="container__quizImage" src={quizImg} alt="logo" />
       <div className='box-text'>Hello World</div>
+      <Button onClick={() => navigate(routePath.TEST)}>Take Test</Button>
     </div>
     <br></br>
     <div className='box'>Hello</div>
+    <Button onClick={() => navigate(routePath.TEST)}>Take Test</Button>
   </div>
   </>;
 };
