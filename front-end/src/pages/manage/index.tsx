3f17a2c5fa073c8695319acf28b891d5140789a3
chieudepzaj <chieumu1999@gmail.com>
2021-12-08 09:48:21 +0700

    manage class update

--- front-end/src/pages/manage/index.tsx
+++ front-end/src/pages/manage/index.tsx
@@ -1,118 +1,135 @@
 import * as React from "react";
+import routePath from 'src/constants/routePath';
+import { useNavigate } from 'react-router-dom';
+
 import { Column, useTable } from "react-table";
+import Header from 'src/layouts/header';
+import { collection, DocumentData, getDocs, Query, query, where } from '@firebase/firestore';
+import { db } from 'src/firebase/firebase';
+import { DbsName } from 'src/constants/db';
+
 import "./styles.scss";
 
 export default function Table() {
-  const data = React.useMemo(
-    () => [
-      {
-        col1: "Nguyen Van A",
+    const data = React.useMemo(
+        () => [
+            {
+                col1: "Nguyen Van A",
 
-        col2: "8"
-      },
+                col2: "8"
+            },
 
-      {
-        col1: "Nguyen Van B",
+            {
+                col1: "Nguyen Van B",
 
-        col2: "9"
-      },
+                col2: "9"
+            },
 
-      {
-        col1: "Nguyen Van C",
+            {
+                col1: "Nguyen Van C",
 
-        col2: "7"
-      },
-      {
-        col1: "Nguyen Van D",
+                col2: "7"
+            },
+            {
+                col1: "Nguyen Van D",
 
-        col2: "10"
-      }
+                col2: "10"
+            }
+        ],
+
+        []
+    );
+
+    const columns = React.useMemo<Column<{ col1: string; col2: string }>[]>(() => [
+        {
+            Header: "Name",
+
+            accessor: "col1" // accessor is the "key" in the data
+        },
+
+        {
+            Header: "Score",
+
+            accessor: "col2"
+        }
     ],
 
-    []
-  );
+        []
+    );
 
-  const columns = React.useMemo<Column<{col1: string;col2: string}>[]>(() => [
-      {
-        Header: "Name",
+    const {
+        getTableProps,
 
-        accessor: "col1" // accessor is the "key" in the data
-      },
+        getTableBodyProps,
 
-      {
-        Header: "Score",
+        headerGroups,
 
-        accessor: "col2"
-      }
-    ],
+        rows,
 
-    []
-  );
+        prepareRow
+    } = useTable({ columns, data });
 
-  const {
-    getTableProps,
+    return (
+        <>
 
-    getTableBodyProps,
+            <div className="background">
+                <Header />
+                <div className="table-container">
+                    <table className="table-content" {...getTableProps()} >
+                        <thead>
+                            {headerGroups.map((headerGroup) => (
+                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                    {headerGroup.headers.map((column) => (
+                                        <th
+                                            {...column.getHeaderProps()}
+                                            style={{
+                                                borderBottom: "solid 1px white",
 
-    headerGroups,
+                                                background: "gray",
 
-    rows,
+                                                color: "white",
 
-    prepareRow
-  } = useTable({ columns, data });
+                                                fontWeight: "bold"
+                                            }}
+                                        >
+                                            {column.render("Header")}
+                                        </th>
+                                    ))}
+                                </tr>
+                            ))}
+                        </thead>
 
-  return (
-    <table {...getTableProps()} className="background">
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  borderBottom: "solid 1px white",
-                  
-                  background: "gray",
+                        <tbody {...getTableBodyProps()}>
+                            {rows.map((row) => {
+                                prepareRow(row);
 
-                  color: "white",
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {row.cells.map((cell) => {
+                                            return (
+                                                <td
+                                                    {...cell.getCellProps()}
+                                                    style={{
+                                                        padding: "10px",
+                                                        borderBottom: "solid 1px white",
 
-                  fontWeight: "bold"
-                }}
-              >
-                {column.render("Header")}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
+                                                        border: "solid 1px white",
 
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
+                                                        background: "gray"
+                                                    }}
+                                                >
+                                                    <a href="/chart-studdent"> {cell.render("Cell")} </a>
+                                                </td>
+                                            );
+                                        })}
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
 
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      padding: "10px",
-                      borderBottom: "solid 1px white",
-                  
-                      border: "solid 1px white",
-
-                      background: "gray"
-                    }}
-                  >
-                    <a href="/chart-studdent"> {cell.render("Cell")} </a>
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+            </div>
+        </>
+    );
 }
