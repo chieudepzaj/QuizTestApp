7f3db7113dddad3a7519d18adbf712f8cd2cef06
unknown <artogirus@gmail.com>
2021-12-28 21:49:41 +0700

    fix 3

--- front-end/src/pages/features/student/manage-test-student/index.tsx
+++ front-end/src/pages/features/student/manage-test-student/index.tsx
@@ -70,8 +70,6 @@ const ManageTestStudent: React.FC = () => {
 
   const handleOnEditQuiz = (quiz: any) => {};
 
-  const handleOnViewQuizResult = (quiz: any) => {};
-
   return (
     <div className="manage-test__container">
       <div className="all-quiz-info-container">
@@ -85,9 +83,6 @@ const ManageTestStudent: React.FC = () => {
               key={index}
               quiz={quiz}
               actions={[
-                <Button key="quiz-result" className="result-btn" onClick={() => handleOnViewQuizResult(quiz)}>
-                  Quiz Results
-                </Button>,
                 <Button key="edit-quiz" className="edit-btn" onClick={() => handleOnEditQuiz(quiz)}>
                   Edit Quiz
                 </Button>,
